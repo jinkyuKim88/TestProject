950fb435dad2351cf575d88d8881b45df2dde2c6
unknown <zkyu@naver.com>
2020-11-25 01:56:20 +0900

    hoho

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main (void)
 {
 	cout << "THIS IS NOT A TEST PROJECT!!!" << "by HB" << endl;
+
 	cout << "yes Dae bu" << endl;
-	
+	cout << "Haengbaeya!" << endl;
+
 	cin.get();
 	return 0;
 }
